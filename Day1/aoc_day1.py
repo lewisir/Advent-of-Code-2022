62f77edd9614703a5745fe368ebc09aa0a149d19
Ian Lewis <ian.r.lewis@bt.com>
2022-12-01 14:26:59 +0000

    Day1 Complete

--- Day1/aoc_day1.py
+++ Day1/aoc_day1.py
@@ -12,19 +12,20 @@ def main():
         for line in file:
             file_data.append(line.rstrip("\n"))
 
-    highest_calorie_count = 0
     current_calorie_count = 0
-
+    elf_calorie_list = []
     for calorie in file_data:
         if calorie == "":
-            if current_calorie_count > highest_calorie_count:
-                highest_calorie_count = current_calorie_count
+            elf_calorie_list.append(current_calorie_count)
             current_calorie_count = 0
         else:
             current_calorie_count += int(calorie)
+    elf_calorie_list.sort(reverse=True)
+    top_three_sum = sum(elf_calorie_list[0:3])
     print(
-        f"Part I - Elf carrying the most calories is carrying {highest_calorie_count} calories"
+        f"Part I - Elf carrying the most calories is carrying {elf_calorie_list[0]} calories"
     )
+    print(f"Part II - Total calories carried by the top three Elves - {top_three_sum}")
 
 
 if __name__ == "__main__":
