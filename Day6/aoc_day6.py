ba6d32608de83c44ff91065e82033421b074cfb7
Ian Lewis <ian.r.lewis@bt.com>
2022-12-06 15:57:38 +0000

    Day 6 (not solved yet, just trying to fix Git)

---
+++ Day6/aoc_day6.py
@@ -0,0 +1,52 @@
+"""
+--- Advent of Code 2022 ---
+--- Day 6: Tuning Trouble ---
+"""
+
+TEST = False
+
+DAY = "6"
+REAL_INPUT = "Day" + DAY + "/input_day" + DAY + ".txt"
+TEST_INPUT = "Day" + DAY + "/input_test.txt"
+
+TEST_STRING = "ababababababcdefghab"
+TEST_STRING_3 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+
+WINDOW = 4
+
+if TEST:
+    FILENAME = TEST_INPUT
+else:
+    FILENAME = REAL_INPUT
+
+def get_input_data(filename):
+    """function to read in the input data"""
+    file_data = []
+    with open(filename) as file:
+        for line in file:
+            file_data.append(line.rstrip("\n"))
+    return file_data
+
+def check_unique_characters(input_string):
+    """check if the input string contains no repeating characters"""
+    for position, char in enumerate(input_string):
+        if char in input_string[position+1:]:
+            return False
+        else:
+            return True
+
+def main():
+    """Main program"""
+    input_data = get_input_data(FILENAME)
+
+    TEST_STRING = input_data[0]
+
+    for position in range(len(TEST_STRING)):
+        sub_string = TEST_STRING[position:position+WINDOW]
+        if check_unique_characters(sub_string):
+            print(f"position for {sub_string} sub_string {position+WINDOW}")
+            break
+
+
+if __name__ == "__main__":
+    main()
